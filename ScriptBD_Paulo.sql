073459e99878528658fb154bac53941a1fbd4c48
pjunior77 <pjunior@centroin.com.br>
2025-11-02 16:48:33 -0300

    commit

--- ScriptBD_Paulo.sql
+++ ScriptBD_Paulo.sql
@@ -1,15 +1,15 @@
-#Cria o Database PROJ_NARA_VAREJO
-CREATE DATABASE PROJ_NARA_VAREJO;
+#Cria o Database PROJETO_NARA_VAREJO
+CREATE DATABASE PROJETO_NARA_VAREJO;
 
 #Seta o Database para uso
-USE PROJ_NARA_VAREJO;
+USE PROJETO_NARA_VAREJO;
 
 ##############################################################
 # Criação das tabelas
 ##############################################################
 
-#Cria a tabela D_clientes ##########################################################
-CREATE TABLE D_clientes (
+#Cria a tabela Clientes ##########################################################
+CREATE TABLE Clientes (
 ID_Cliente INT,
 Nome VARCHAR(100),
 Idade INT,
@@ -21,8 +21,8 @@ Perfil VARCHAR(50),
 PRIMARY KEY (ID_Cliente)
 );
 
-#Cria a tabela D_produtos ##########################################################
-CREATE TABLE D_produtos (
+#Cria a tabela Produtos ##########################################################
+CREATE TABLE Produtos (
 ID_Produto INT,
 Nome_Produto VARCHAR(100),
 Categoria VARCHAR(50),
@@ -31,8 +31,8 @@ Marca VARCHAR(50),
 PRIMARY KEY (ID_Produto)
 );
 
-#Cria a tabela D_campanhas ##########################################################
-CREATE TABLE D_campanhas (
+#Cria a tabela Campanhas ##########################################################
+CREATE TABLE Campanhas (
 ID_Campanha INT,
 Canal VARCHAR(50),
 Data_Inicio DATE,
@@ -42,8 +42,8 @@ Custo DECIMAL(10,2),
 PRIMARY KEY(ID_Campanha)
 );
 
-#Cria a tabela F_atendimentos ##########################################################
-CREATE TABLE F_atendimentos (
+#Cria a tabela Atendimentos ##########################################################
+CREATE TABLE Atendimentos (
 ID_Atendimento INT,
 ID_Cliente INT,
 Tipo VARCHAR(50),
@@ -51,11 +51,11 @@ Data DATE,
 Tempo_Resposta DECIMAL(10,2),
 Nota_Satisfacao INT,
 PRIMARY KEY (ID_Atendimento),
-FOREIGN KEY (ID_Cliente) REFERENCES D_clientes(ID_Cliente)
+FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente)
 );
 
-#Cria a tabela F_avaliacoes ##########################################################
-CREATE TABLE F_avaliacoes (
+#Cria a tabela Avaliacoes ##########################################################
+CREATE TABLE Avaliacoes (
 ID_Avaliacao INT AUTO_INCREMENT,
 ID_Cliente INT,
 ID_Produto INT,
@@ -63,12 +63,12 @@ Nota INT,
 Data_Avaliacao DATE,
 Comentario VARCHAR(50),
 PRIMARY KEY (ID_Avaliacao),
-FOREIGN KEY (ID_Cliente) REFERENCES D_clientes(ID_Cliente),
-FOREIGN KEY (ID_Produto) REFERENCES D_produtos(ID_Produto)
+FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente),
+FOREIGN KEY (ID_Produto) REFERENCES Produtos(ID_Produto)
 );
 
-#Cria a tabela F_vendas ##########################################################
-CREATE TABLE F_vendas (
+#Cria a tabela Vendas ##########################################################
+CREATE TABLE Vendas (
 ID_Venda INT,
 ID_Cliente INT,
 ID_Produto INT,
@@ -78,9 +78,9 @@ Canal VARCHAR(50),
 Valor_Total DECIMAL(10,2),
 ID_Campanha INT,
 PRIMARY KEY (ID_Venda),
-FOREIGN KEY (ID_Cliente) REFERENCES D_clientes(ID_Cliente),
-FOREIGN KEY (ID_Produto) REFERENCES D_produtos(ID_Produto),
-FOREIGN KEY (ID_Campanha) REFERENCES D_campanhas(ID_Campanha)
+FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente),
+FOREIGN KEY (ID_Produto) REFERENCES Produtos(ID_Produto),
+FOREIGN KEY (ID_Campanha) REFERENCES Campanhas(ID_Campanha)
 );
 
 
@@ -98,10 +98,10 @@ SET GLOBAL local_infile = 1;				#Permissão para acessar arquivos locais no serv
 #	ProcessaArqVendasExtrasParaBD_Paulo.ipynb
 ######################################################################################################
 
-#Carrega os dados da tabela D_clientes ##########################################################
+#Carrega os dados da tabela Clientes ##########################################################
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/dfClientes.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/dfClientes.csv'
-INTO TABLE D_clientes
+INTO TABLE Clientes
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -114,12 +114,12 @@ Estado,
 Canal_Aquisicao,
 Perfil
 );
-#SELECT * FROM D_clientes;
+#SELECT * FROM Clientes;
 
-#Carrega os dados da tabela D_produtos ##########################################################
+#Carrega os dados da tabela Produtos ##########################################################
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/produtos.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/produtos.csv'
-INTO TABLE D_produtos
+INTO TABLE Produtos
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -129,12 +129,12 @@ Categoria,
 Preco,
 Marca
 );
-#SELECT * FROM D_produtos;
+#SELECT * FROM Produtos;
 
-#Carrega os dados da tabela D_campanhas ##########################################################
+#Carrega os dados da tabela Campanhas ##########################################################
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/campanhas_corrigido.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/campanhas_corrigido.csv'
-INTO TABLE D_campanhas
+INTO TABLE Campanhas
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -145,12 +145,12 @@ Data_Fim,
 Tipo_Campanha,
 Custo
 );
-#SELECT * FROM D_campanhas;
+#SELECT * FROM Campanhas;
 
-#Carrega os dados da tabela F_atendimentos ##########################################################
+#Carrega os dados da tabela Atendimentos ##########################################################
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/dfAtendimentos.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/dfAtendimentos.csv'
-INTO TABLE F_atendimentos
+INTO TABLE Atendimentos
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -161,12 +161,12 @@ Data,
 Tempo_Resposta,
 Nota_Satisfacao
 );
-#SELECT * FROM F_atendimentos;
+#SELECT * FROM Atendimentos;
 
-#Carrega os dados da tabela F_avaliacoes ##########################################################
+#Carrega os dados da tabela Avaliacoes ##########################################################
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/avaliacoes.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/avaliacoes.csv'
-INTO TABLE F_avaliacoes
+INTO TABLE Avaliacoes
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -176,12 +176,12 @@ Nota,
 Data_Avaliacao,
 Comentario
 );
-#SELECT * FROM F_avaliacoes;
+#SELECT * FROM Avaliacoes;
 
-#Carrega os dados da tabela F_vendas ##########################################################
+#Carrega os dados da tabela Vendas ##########################################################
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/dfVendas.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/dfVendas.csv'
-INTO TABLE F_vendas
+INTO TABLE Vendas
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -194,11 +194,11 @@ Canal,
 Valor_Total,
 ID_Campanha
 );
-#SELECT * FROM F_vendas;
+#SELECT * FROM Vendas;
 
 LOAD DATA INFILE 'C:/PauloFrederico/PosGraduacao/SENAC_BigData/Projeto_Nara_Varejo/dfVendasExtras.csv'
 #LOAD DATA INFILE 'C:/Users/paulo.frederico/Documents/Projeto_Nara_Varejo/dfVendasExtras.csv'
-INTO TABLE F_vendas
+INTO TABLE Vendas
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS						#Ignora a primeira linha por causa do cabeçalho
@@ -211,12 +211,12 @@ Canal,
 Valor_Total,
 ID_Campanha
 );
-SELECT COUNT(*) FROM F_Vendas;
+
 /*
-DROP TABLE D_clientes;
-DROP TABLE D_produtos;
-DROP TABLE D_campanhas;
-DROP TABLE F_atendimentos;
-DROP TABLE F_avaliacoes;
-DROP TABLE F_vendas;
+DROP TABLE Clientes;
+DROP TABLE Produtos;
+DROP TABLE Campanhas;
+DROP TABLE Atendimentos;
+DROP TABLE Avaliacoes;
+DROP TABLE Vendas;
 */
